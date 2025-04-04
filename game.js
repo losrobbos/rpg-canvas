7bed5dedeb83ae67843f6ae1dbd7d3b8ffe7393b
losrobbos <losrobbos@web.de>
2025-04-04 13:38:24 +0200

    GameLoop implementiert und Sprite animation je nach Direction des Players

--- game.js
+++ game.js
@@ -26,58 +26,104 @@ const mapOffset = {
   y: -200,
 };
 
-// image.onload event is fired when
-// image is fully loaded and accessible
-// and therefore: drawable
-imgBg.onload = () => {
+const TILE_SIZE = 64;
+// spritesheet tile size berechnung
+// sheet width = 832
+// sheet height = 1344
+// anzahl sheet spalten = 13
+// anzahl sheet reihen = 21
+// tile größe => breite / spalten = 64x64px
 
-  // this method is nice to fill the whole canvas with out image
-  // this might be useful if image and canvas have same aspect ratio and
-  // we want to see the FULL background, not just a part
-  // Beispiel: Zelda NES
-  // ctx.drawImage(imgBg, 0, 0, canvas.width, canvas.height)
-
-  imgPlayer.onload = () => {
-    const TILE_SIZE = 64;
-    // spritesheet tile size berechnung
-    // sheet width = 832
-    // sheet height = 1344
-    // anzahl sheet spalten = 13
-    // anzahl sheet reihen = 21
-    // tile größe => breite / spalten = 64x64px
-
-    const rowAnimation = 10;
-
-    const playerPositionInitial = {
-      x: 285,
-      y: 190,
-    };
-
-    for(let colSprite=0; colSprite<=8; colSprite++) {
-
-      setTimeout(() => {
-
-        // zeichne background
-        ctx.drawImage(imgBg, mapOffset.x, mapOffset.y);
-
-        // zeichne playersprite (=ein einzelbild aus spritesheet)
-        ctx.drawImage(
-          imgPlayer,
-          // position von sprite (=einzelbild) im spritesheet
-          colSprite * TILE_SIZE,
-          rowAnimation * TILE_SIZE,
-          // width & height des Tiles = 64px
-          TILE_SIZE,
-          TILE_SIZE,
-          // position im canvas
-          playerPositionInitial.x,
-          playerPositionInitial.y,
-          // größe im canvas
-          TILE_SIZE,
-          TILE_SIZE
-        );
-      }, 100 * colSprite);
-    }
-  };
+const playerPositionInitial = {
+  x: 285,
+  y: 190,
 };
 
+/** @type {"up" | "down" | "right" | "left"} */
+let direction = undefined
+let rowAnimation = 10;
+
+window.addEventListener("keydown", (e) => {
+  switch(e.key) {
+    case "ArrowUp": 
+      direction = "up"; break;
+    case "ArrowDown": 
+      direction = "down"; break;
+    case "ArrowLeft": 
+      direction = "left"; break;
+    case "ArrowRight": 
+      direction = "right"; break;
+  }
+})
+
+window.addEventListener("keyup", (e) => {
+  switch(e.key) {
+    case "ArrowUp": 
+    case "ArrowDown": 
+    case "ArrowLeft": 
+    case "ArrowRight": 
+      direction = undefined;
+  }
+})
+
+// zähle wie oft schon ein bild gezeichnet wurde
+let frames = 0;
+let frameIndex = 0;
+
+const gameLoop = () => {
+
+  // set animation row in sheet based on DIRECTION player chose
+  switch(direction) {
+    case "up": 
+      rowAnimation = 8; 
+      mapOffset.y++;
+      break;
+    case "left": 
+      mapOffset.x++;
+      rowAnimation = 9; 
+      break;
+    case "down": 
+      rowAnimation = 10; 
+      mapOffset.y--;
+      break;
+    case "right": 
+      mapOffset.x--;
+      rowAnimation = 11; 
+      break;
+  }
+
+  // bestimme frame position der animation
+  if(frames % 6 === 0) {
+    frameIndex = frameIndex < 8 ? frameIndex +1 : 0
+  }
+
+  // zeichne background
+  ctx.drawImage(imgBg, mapOffset.x, mapOffset.y);
+
+  // zeichne playersprite (=ein einzelbild aus spritesheet)
+  ctx.drawImage(
+    imgPlayer,
+    // position von sprite (=einzelbild) im spritesheet
+    frameIndex * TILE_SIZE,
+    rowAnimation * TILE_SIZE,
+    // width & height des Tiles = 64px
+    TILE_SIZE,
+    TILE_SIZE,
+    // position im canvas
+    playerPositionInitial.x,
+    playerPositionInitial.y,
+    // größe im canvas
+    TILE_SIZE,
+    TILE_SIZE
+  );
+
+  frames++;
+  console.log({frames})
+
+  // request animation frame methode wartet
+  // bis der f*** monitor wieder bereit ist ein bild zu zeichnen
+  requestAnimationFrame(gameLoop);
+};
+
+// start game loop
+gameLoop();
